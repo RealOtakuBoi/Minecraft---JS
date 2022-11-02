f41e3e5b4c6bb50468521d3c472e379a7a845c9c
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-02 13:28:21 +0530

    Keyboard hook set up

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -2,8 +2,11 @@ import { useSphere } from "@react-three/cannon"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useEffect, useRef } from "react"
 import { Vector3 } from "three"
+import { UseKeyboard } from "../Hooks/UseKeyboard"
+
 
 export const Player = () => {
+    const actions = UseKeyboard()
     const { camera } = useThree()
     const [ref, api] = useSphere(() => ({
         mass: 1,
@@ -25,7 +28,7 @@ export const Player = () => {
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0] , pos.current[1], pos.current[2]))
 
-        api.velocity.set(0, 0 , 0)
+        // api.velocity.set(0, 0 , 0)
     })
 
     return(
