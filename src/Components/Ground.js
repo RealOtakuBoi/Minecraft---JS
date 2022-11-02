221541e45bdd3ffcb261145ba8668036246c5dab
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-02 12:52:12 +0530

    ground and sky done

---
+++ src/Components/Ground.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { usePlane } from '@react-three/cannon'
+import {groundTexture} from '../images/Textures'
+import { NearestFilter, RepeatWrapping } from 'three'
+
+const Ground = () => {
+  const [ref] = usePlane(() => ({
+    rotation: [-Math.PI / 3, 0, 0], position: [0, 0, 0]
+  }))
+
+  groundTexture.magFilter = NearestFilter
+  groundTexture.wrapS = RepeatWrapping
+  groundTexture.wrapT = RepeatWrapping
+  groundTexture.repeat.set(100 , 100)
+
+  return(
+    <mesh ref={ref}>
+        <planeBufferGeometry attach = 'geometry' args ={[100, 100]} />
+        <meshStandardMaterial attach='material' map={groundTexture} />
+    </mesh>
+  )
+}
+
+export default Ground
