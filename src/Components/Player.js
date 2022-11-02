06e5502d76441d615db5163ed291ef280a29daa8
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-02 13:13:10 +0530

    jesus fly bug fixed

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -8,17 +8,24 @@ export const Player = () => {
     const [ref, api] = useSphere(() => ({
         mass: 1,
         type: "Dynamic",
-        position: [0,0,0]
+        position: [0,2,0]
     }))
 
-    const pos = useRef([0,0,0])
+    const vel = useRef([0,0,0])
+    useEffect(() => {
+        api.velocity.subscribe((v) => vel.current = v)
+    } , [api.velocity] )
 
+
+    const pos = useRef([0,0,0])
     useEffect(() => {
         api.position.subscribe((p) => pos.current = p)
     } , [api.position] )
 
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0] , pos.current[1], pos.current[2]))
+
+        api.velocity.set(0, 0 , 0)
     })
 
     return(
