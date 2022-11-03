a02812225fd0219938ad7f3444ad9462b8ba120e
RealOtakuBoi <sohamseth2005@gmail.com>
2022-11-03 11:34:30 +0530

    state management for adding cubes done

---
+++ src/Hooks/useStore.js
@@ -0,0 +1,49 @@
+import create from 'zustand'
+import { nanoid } from 'nanoid'
+
+
+
+export const useStore = create((set) => ({
+    texture: 'dirt',
+    cubes: [
+        {
+        key: nanoid(),
+        pos: [10,0.5,10],
+        texture : 'dirt',
+        },
+
+        {
+            key: nanoid(),
+            pos: [2,0.5,5],
+            texture : 'wood',
+        }
+        
+
+],
+
+    addCube: (x,y,z) => {
+        set((prev) => ({
+            cubes : [
+                ...prev.cubes,
+                {
+                    key: nanoid(),
+                    pos: [x,y,z],
+                    texture: prev.texture
+
+                }
+            ]
+        }))
+    },
+
+
+    removeCube: () => {},
+
+
+    setTexture: () => {},
+
+
+    saveWorld: () => {},
+
+
+    resetWorld: () => {}
+}))
